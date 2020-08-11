6c7ac978c2aa01baed6ec19d1ce496f4336b83ba
Roopesh V S <txtmeroopesh@gmail.com>
2020-08-11 15:28:20 +0530

    Updates

---
+++ precache-manifest.a98e1c76b267936e558161cb47643636.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c521fa2376927c6d573837b579a7aa",
+    "url": "/roopeshvs/React-Student-Scores-Chart/index.html"
+  },
+  {
+    "revision": "6059e2d49e7d5bc6239e",
+    "url": "/roopeshvs/React-Student-Scores-Chart/static/css/main.49a8f50c.chunk.css"
+  },
+  {
+    "revision": "e3951f8c3eaee9abea5d",
+    "url": "/roopeshvs/React-Student-Scores-Chart/static/js/2.597147c3.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/roopeshvs/React-Student-Scores-Chart/static/js/2.597147c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6059e2d49e7d5bc6239e",
+    "url": "/roopeshvs/React-Student-Scores-Chart/static/js/main.8b02699a.chunk.js"
+  },
+  {
+    "revision": "d44c6f5502bf18b9d5e4",
+    "url": "/roopeshvs/React-Student-Scores-Chart/static/js/runtime-main.2ae3e48c.js"
+  }
+]);
